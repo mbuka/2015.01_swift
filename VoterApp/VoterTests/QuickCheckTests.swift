d14094d60b93c0c8f9033b701a0ca62ee37bcb3f
mbuka <marek3131@o2.pl>
2015-02-04 18:12:13 +0100

    add basic quick check median test

--- VoterApp/VoterTests/QuickCheckTests.swift
+++ VoterApp/VoterTests/QuickCheckTests.swift
@@ -19,13 +19,48 @@ func Check<X: Arbitrary, Y: Arbitrary>(prop: (X, Y) -> Bool, desc: String) {
     XCTAssert(check(desc, prop), desc)
 }
 
-class QuickCheckTests: XCTestCase {
-    func testMath() {
-        // plus is cummutative
+func Check<X: Arbitrary>(prop: [X] -> Bool, desc: String) {
+    XCTAssert(check(desc, prop), desc)
+}
+
+extension Vote: Arbitrary{
+    static func arbitrary() -> Vote {
+        let rand = arc4random() % 3;
+        switch(rand){
+        case 0:
+            return Vote.Hate
+        case 1:
+            return Vote.Like
+        case 2:
+            return Vote.Neutral
+        default:
+            return Vote.Neutral
+        }
     }
     
-    func testVoteManager() {
-        
+    func smaller() -> Vote? { return nil }
+}
+
+
+class QuickCheckTests: XCTestCase {
+    func testMath() {
+        Check(plusIsCommutative, "Error in plus")
+    }
+    
+    func testVoteManagerMedian() {
+        Check(voteManagerReturnsNones, "Error in none median")
     }
     
 }
+
+func plusIsCommutative(x: Int, y: Int) -> Bool {
+    return x + y == y + x
+}
+
+
+
+func voteManagerReturnsNones(voteArray: Array<Vote>) -> Bool{
+    return true
+}
+
+
